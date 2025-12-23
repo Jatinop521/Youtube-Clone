3f443cd5c6ffe3c250c866faac0e9883a2025e04
JatinRathiJaat <abc.gmail>
2025-12-24 04:56:28 +0530

    Error typo corrected

--- src/utils/errorApi.js
+++ src/utils/errorApi.js
@@ -1,13 +1,13 @@
 class ErrorApi extends Error {
-    constructor(statuscode , message= 'Something went wrong' , errors = [] , statck = ""){
+    constructor(statuscode , message= 'Something went wrong' , errors = [] , stacks = ""){
         super(message);
         this.statuscode = statuscode;
         this.errors = errors;
         this.success = false;
         this.data = false;
 
-        if(statck){
-            this.stack = statck;
+        if(stacks){
+            this.stack = stacks;
         }else{
             Error.captureStackTrace(this , this.constructor);
         }
